465da603deb19725baf53e7ae14de3c368b0ec34
jacob <15millerj@ellesmere.school.nz>
2020-06-11 12:11:19 +1000

    ah yes 2

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
 
 @app.errorhandler(404)
 def error404(e):
-    # note that we set the 404 status explicitly
     return render_template('404.html'), 404
