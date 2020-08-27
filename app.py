83849bd3057e8c6721fb007535405f3fa4b349d1
jacob <15millerj@ellesmere.school.nz>
2020-08-27 13:32:10 +1200

    firebace is a bitch

--- app.py
+++ app.py
@@ -1,21 +1,5 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/Home")
-def home():
-    return render_template('home.html')
-
-@app.errorhandler(404)
-def error404(e):
-    return render_template('404.html'), 404
-
-@app.route("/Login")
-def Login():
-    return render_template('Login.html')
-
-@app.route("/Registration")
-def Registration():
-    return render_template('Registration.html')
+import app as app
 
 if __name__ == '__main__':
-  app.run(debug=True)
-
+    run_app = app.create_app()
+    run_app.run(debug=True) 
