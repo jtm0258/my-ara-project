01794a7e2ea100de6be2a62a8bac425ca1b18cfd
jacob <15millerj@ellesmere.school.nz>
2020-10-22 14:03:44 +1300

    git commit -m

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -34,14 +34,28 @@ $(document).ready(function(){
 		gutter: 30
 	});
 
+	if ($('#filter-select').length > 0 ) {
+		var filter = $('#filter-select').data('filter');
+		$('#filter-select').val(filter);
+	}
+	
+	if ($('#category').length > 0 ) {
+		var filter = $('#category').data('category');
+		$('#category').val(filter);
+	}
 
-	$('.grid-item').click(function(){
-		var image_data = $(this).data(image);
+	$('.grid-item figure').click(function(){
+		//Fetch image data
+		var image_data = $(this).closest('.grid-item').data(image);
 		var image = image_data.image;
+	
+		//Build HTML from image data
 		var description = `<p>${image.description}</p>`;
-		var title = `<h5 class="modal-title">${image.name}<i class="fa fa-times" data-dismiss="modal" aria-label="Close" aria-hidden="true"></i></h5>`;
-		var img = `<img src="${image.upload_location}" alt="${image.name}">`;
+		var title = `<h5 class="modal-title">${image.name}<i class="fa fa-times" class="close" data-dismiss="modal" aria-label="Close" aria-hidden="true"></i></h5>`;
+		var img = `<img src="${image.upload_location}" alt="${image.name}" />`;
+	
+		//Add to modal and open modal
 		$('#image-modal .modal-body').html(img + title + description);
 		$('.modal').modal('show');
-	});	
+	});
 });
